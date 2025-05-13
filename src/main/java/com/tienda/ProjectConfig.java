01393380f03701e3c5bb5b1b4b64ba70883154b0
Laboratorios <Laboratorios@172.16.11.6>
2025-05-12 20:42:26 -0600

    semana 1 git

---
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -0,0 +1,16 @@
+package com.tienda;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro) {
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/ejemplo2").setViewName("ejemplo2");
+    }
+    
+}
